1b5609b46297ae75f30db9261bacf0fe7cf32aa3
NahimaSA <146004756+Chloxhrb@users.noreply.github.com>
2025-06-04 16:22:42 +0200

    fix: added main page to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+// Pages
+import Home from "./pages/Home.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
